c032f7d75d4be0b4d8f8aee0d6cad19321051595
Alan San <alansandovalt1@gmail.com>
2025-04-23 16:15:08 -0600

    feat: agregar API de clima para una respuesta ams precisa

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from "zod";
+import { date, z } from "zod";
 
 // 1. crear el servidor
 // es la interfaz principal con el protocolo MCP. MANEJA LA CONMUNICACION ENTRE EL CLIENTE Y EL SERVIDOR
@@ -8,8 +8,6 @@ const server = new McpServer({
   name: "Demo",
   version: "1.0.0",
 });
-
-
 
 // 2. herramientas
 server.tool(
@@ -19,13 +17,52 @@ server.tool(
   {
     city: z.string().describe("City name"),
   },
+  //   ejemplo 1
   // y aqui le decimos que haga lo que quermampos con la informacion  que el cliente nos manda
+  //   async ({ city }) => {
+  //     return {
+  //       content: [
+  //         {
+  //           type: "text",
+  //           text: `El clima de ${city} es soleado`,
+  //         },
+  //       ],
+  //     };
+  //   }
+
+
+
+
+  //   ejemplo 2
+  // conectarnos a una API de clima y obtener el clima en tiempo real
   async ({ city }) => {
+    const infoCity = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
+    );
+    const dataCity = await infoCity.json();
+    if (dataCity.length === 0) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `No se encontraron datoss sobre ${city}`,
+          },
+        ],
+      };
+    }
+    const respuestsaCity = dataCity.results[0];
+    const { latitude, longitude } = respuestsaCity;
+    console.log(respuestsaCity);
+    const infoWeather = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain,weather_code,apparent_temperature`
+    );
+    const dataWeather = await infoWeather.json();
+
     return {
       content: [
         {
           type: "text",
-          text: `El clima de ${city} es soleado`,
+          text: JSON.stringify(dataWeather, null, 2),
         },
       ],
     };
@@ -34,7 +71,7 @@ server.tool(
 
 
 
-// 3.  escuchar las conexiones del cliente 
-// creamos el trnasporete en nuestra maquina, no se tiene que conectar a interrnet ni nada 
+// 3.  escuchar las conexiones del cliente
+// creamos el trnasporete en nuestra maquina, no se tiene que conectar a interrnet ni nada
 const transporte = new StdioServerTransport();
 await server.connect(transporte);
